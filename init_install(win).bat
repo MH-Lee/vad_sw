828f8ceebdcb3100ba087b3ffb55f9b1afd41749
MH-Lee <lmhoon012@gmail.com>
2021-03-03 16:18:27 +0900

    win bat add

--- init_install(win).bat
+++ init_install(win).bat
@@ -2,3 +2,11 @@ python -m venv ./vad_venv
 call .\vad_venv\Scripts\activate.bat
 python -m pip install --upgrade pip
 pip install -r requirements.txt
+
+python -m pip install --upgrade pyqt5
+python -m pip install pyqt5-tools
+
+echo %USERNAME%
+setx QT_QPA_PLATFORM_PLUGIN_PATH "C:\Users\%USERNAME%\Anaconda3\Lib\site-packages\PyQt5\Qt\plugins"
+
+pause
